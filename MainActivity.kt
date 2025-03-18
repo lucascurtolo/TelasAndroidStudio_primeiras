105e3763ad201493ea21cd2d50fef5c5ab717799
lucascurtolo <161758944+lucascurtolo@users.noreply.github.com>
2025-03-17 22:42:54 -0300

    telas principais

---
+++ MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tela
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lateinit var buttonT1: Button
+
+        setContentView(R.layout.activity_main)
+        
+    }
+}
